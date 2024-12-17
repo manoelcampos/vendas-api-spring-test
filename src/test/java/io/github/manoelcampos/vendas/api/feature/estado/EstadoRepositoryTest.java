44c597486fc86c35eae6930cd2ede5b8a19afb81
Manoel Campos <manoelcampos@gmail.com>
2024-12-17 11:33:35 -0300

    Cria novos testes no EstadoRepositoryTest
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/test/java/io/github/manoelcampos/vendas/api/feature/estado/EstadoRepositoryTest.java
+++ src/test/java/io/github/manoelcampos/vendas/api/feature/estado/EstadoRepositoryTest.java
@@ -1,12 +1,14 @@
 package io.github.manoelcampos.vendas.api.feature.estado;
 
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * A anotação @DataJpaTest é usada para testes de repositórios.
@@ -15,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 @DataJpaTest
 class EstadoRepositoryTest {
+    public static final String NOME_ESPERADO = "Novo Estado";
     /* Anotação @Autowired para injetar a instância do repositório.
     * Ou seja, ela cria uma instância do objeto pra gente. */
     @Autowired
@@ -23,19 +26,73 @@ class EstadoRepositoryTest {
     @Test
     void findById() {
         final long id = 1;
-        Estado estado = repository.findById(id).orElseThrow();
         final var nomeEsperado = "São Paulo";
+        findById(id, nomeEsperado);
+    }
+
+    private void findById(long id, String nomeEsperado) {
+        Estado estado = repository.findById(id).orElseThrow();
         assertEquals(nomeEsperado, estado.getDescricao());
     }
 
     @Test
-    void deleteById() {
+    void insereComSucesso() {
+        final long id = cadastrarEstado();
+        System.out.println(id);
+        assertNotEquals(0, id);
+        findById(id, NOME_ESPERADO);
+    }
+
+    /**
+     * Insere um estado no banco
+     * @return o ID do estado inserido
+     * @throws ConstraintViolationException caso a validação dos dados do estado falhe e o estado não seja inserido
+     */
+    private long cadastrarEstado() {
+        final var estado = new Estado();
+        estado.setDescricao(NOME_ESPERADO);
+        estado.setSigla("NE");
+        final var novoEstado = repository.save(estado);
+
+        /* Usa requireNonNullElse para retornar zero caso o ID seja null.
+        * Mas esta é apenas uma verificação para o IDE não reclamar em outros pontos
+        * que o ID pode ser nulo.
+        * O ID só seria nulo se o estado não for inserido, que vai dar erro na linha
+        * de cima e nem chega aqui. */
+        return Objects.requireNonNullElse(novoEstado.getId(), 0L);
+    }
+
+    @Test
+    void erroAoInserirSemDescricao() {
+        final var estado = new Estado();
+        estado.setSigla("NE");
+        assertThrows(ConstraintViolationException.class, () -> repository.save(estado));
+    }
+
+
+    @Test
+    void deleteByIdExcluiCadastro() {
+        final long id = cadastrarEstado();
+        repository.deleteById(id);
+
+        // Envia o comando SQL imediatamente para o banco
+        repository.flush();
+
+        assertTrue(repository.findById(id).isEmpty());
+    }
+
+    @Test
+    void deleteByIdViolaChaveEstrangeira() {
         final long id = 1; // São Paulo
 
-        assertThrows(DataIntegrityViolationException.class, () -> {
+        var exception = assertThrows(DataIntegrityViolationException.class, () -> {
             repository.deleteById(id);
-            // Envia o comando SQL imediatemente para o banco
+            // Envia o comando SQL imediatamente para o banco
             repository.flush();
         });
+
+        final var constraintName = "FK_CIDADE__ESTADO";
+        assertTrue(exception.getMessage().contains(constraintName));
     }
+
 }
