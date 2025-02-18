a63f3cedcf15677f92c69b73ae5e1851861ce672
Manoel Campos <manoelcampos@gmail.com>
2025-02-18 11:18:42 -0300

    Adiciona TODO no ClienteService
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/io/github/manoelcampos/vendas/api/feature/cliente/ClienteService.java
+++ src/main/java/io/github/manoelcampos/vendas/api/feature/cliente/ClienteService.java
@@ -8,4 +8,17 @@ public class ClienteService extends AbstractCrudService<Cliente, ClienteReposito
     public ClienteService(final ClienteRepository repository) {
         super(repository);
     }
+
+    /*
+    TODO: Incluir campo senha (hash) e ao alterar um cliente,
+    não permitir alterar a senha,
+    pois isso normalmente é feito separadamente:
+    Desta forma, permite-se que o cliente altere seus dados pessoais,
+    sem necessariamente ter que alterar a senha.
+    E se ele não informar a senha ao alterar seus dados,
+    o campo senha virá vazio, substituindo a senha atual.
+    Assim, o usuário não conseguiria logar novamente.
+    Isso pode ser feito com @Column(updatable = false).
+    Mas vamos fazer manualmente para praticar a implementação de regras de negócio.
+    */
 }
