145269d59b282142c538bf8c9c2c73b1bd690ff8
Manoel Campos <manoelcampos@gmail.com>
2025-03-12 17:53:36 -0300

    docs: Converte docs de AbstractControllerIntegrationTest para Markdown
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/test/java/io/github/manoelcampos/vendas/api/feature/AbstractControllerIntegrationTest.java
+++ src/test/java/io/github/manoelcampos/vendas/api/feature/AbstractControllerIntegrationTest.java
@@ -7,17 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-/**
- * Classe abstrata para a criação de testes de integração de APIs REST,
- * que não usa mocks, testando todas as camadas da aplicação de forma integrada.
- *
- * @author Manoel Campos
- * @see <a href="https://www.baeldung.com/spring-5-webclient">WebClient and WebClientTest</a>
- * @see <a href="https://docs.spring.io/spring-framework/reference/testing/webtestclient.html#webtestclient-context-config">Spring WebClientTest</a>
- * @see <a href="https://34codefactory.medium.com/spring-5-webclient-and-webtestclient-tutorial-code-factory-84e32978149a">Spring WebClientTest Tutorial</a>
- * @see <a href="https://rieckpil.de/spring-webtestclient-for-efficient-testing-of-your-rest-api/">testing-of-your-rest-api</a>
- * @see AbstractControllerTest
- */
+/// Classe abstrata para a criação de testes de integração de APIs REST,
+/// que não usa mocks, testando todas as camadas da aplicação de forma integrada.
+///
+/// @author Manoel Campos
+/// @link [WebClient and WebClientTest](https://www.baeldung.com/spring-5-webclient)
+/// @link [Spring WebClientTest](https://docs.spring.io/spring-framework/reference/testing/webtestclient.html#webtestclient-context-config)
+/// @link [Spring WebClientTest Tutorial](https://34codefactory.medium.com/spring-5-webclient-and-webtestclient-tutorial-code-factory-84e32978149a)
+/// @link [Testing of your rest api](https://rieckpil.de/spring-webtestclient-for-efficient-testing-of-your-rest-api/)
+/// @see AbstractControllerTest
 @Getter @Accessors(fluent = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractControllerIntegrationTest {
