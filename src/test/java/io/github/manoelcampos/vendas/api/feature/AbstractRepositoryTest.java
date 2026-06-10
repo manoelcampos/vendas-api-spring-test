700cad3e4a83c861dfd07ae76fff79505da3925c
Manoel Campos <manoelcampos@gmail.com>
2026-06-10 20:39:14 -0300

    Corrige typos
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/test/java/io/github/manoelcampos/vendas/api/feature/AbstractRepositoryTest.java
+++ src/test/java/io/github/manoelcampos/vendas/api/feature/AbstractRepositoryTest.java
@@ -16,7 +16,7 @@ public abstract class AbstractRepositoryTest {
      * Verifica se uma exceção lançada para uma operação de banco de dados
      * violou uma constraint específica.
      * @param ex exceção gerada
-     * @param constraintName nome da constrainta que espera-se que tenha sido violada (como uma UC ou FK)
+     * @param constraintName nome da constraint que se espera que tenha sido violada (como uma UC ou FK)
      */
     protected static void assertConstraintViolation(final DataIntegrityViolationException ex, final String constraintName) {
         final var msg = "O nome da constraint esperada de ser violada não foi identificado: " + ex.getMessage();
